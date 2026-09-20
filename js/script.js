a8f225aaba7046ca752b998bc0938d92d95d6ec3
Анатолій <visockijanatolij1@gmail.com>
2026-09-20 04:15:22 +0200

    finish dropdown section

--- js/script.js
+++ js/script.js
@@ -1,15 +1,34 @@
 const menu = document.querySelector(".menu");
 const burger = document.querySelector(".burger");
 
+const productDropdownButton = document.querySelector(".product-dropdown_button");
+const productDropdownMenu = document.querySelector(".product-dropdown_menu");
+
+const companyDropdownButton = document.querySelector(".company-dropdown_button");
+const companyDropdownMenu = document.querySelector(".company-dropdown_menu");
+
+const connectDropdownButton = document.querySelector(".connect-dropdown_button");
+const connectDropdownMenu = document.querySelector(".connect-dropdown_menu");
+
 burger.addEventListener("click", () => {
     burger.classList.toggle("active");
     menu.classList.toggle("active");
 });
 
+productDropdownButton.addEventListener("click", () => {
+    productDropdownMenu.classList.toggle("active");
+    productDropdownButton.classList.toggle("active");
+});
 
+companyDropdownButton.addEventListener("click", () => {
+    companyDropdownMenu.classList.toggle("active");
+    companyDropdownButton.classList.toggle("active");
+});
 
-
-
+connectDropdownButton.addEventListener("click", () => {
+    connectDropdownMenu.classList.toggle("active");
+    connectDropdownButton.classList.toggle("active");
+});
 
 
 
